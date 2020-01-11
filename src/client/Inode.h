c68755fc11327476d888f5563692021d0c702c5c
Yan, Zheng <zyan@redhat.com>
2020-01-11 15:58:52 +0800

    client: wait for async creating before sending request or cap message
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/client/Inode.h
+++ src/client/Inode.h
@@ -113,12 +113,12 @@ struct AsyncCreateStat {
 };
 
 // inode flags
-#define I_COMPLETE	1
-#define I_DIR_ORDERED	2
-#define I_CAP_DROPPED	4
-#define I_SNAPDIR_OPEN	8
-#define I_KICK_FLUSH	16
-
+#define I_COMPLETE		(1 << 0)
+#define I_DIR_ORDERED		(1 << 1)
+#define I_CAP_DROPPED		(1 << 2)
+#define I_SNAPDIR_OPEN		(1 << 3)
+#define I_KICK_FLUSH		(1 << 4)
+#define I_ASYNC_CREATING	(1 << 5)
 
 struct Inode {
   Client *client;
