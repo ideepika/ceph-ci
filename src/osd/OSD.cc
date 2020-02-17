ceph/ceph

osd: take heartbeat_lock when calling heartbeat()
Fixes: http://tracker.ceph.com/issues/39439
Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/OSD.cc
@@ -5212,6 +5212,7 @@ void OSD::heartbeat_check()
 
 void OSD::heartbeat()
 {
+  ceph_assert(heartbeat_lock.is_locked_by_me());
   dout(30) << "heartbeat" << dendl;
 
   // get CPU load avg
@@ -5861,7 +5862,10 @@ void OSD::_preboot(epoch_t oldest, epoch_t newest)
 	   << oldest << ".." << newest << dendl;
 
   // ensure our local fullness awareness is accurate
-  heartbeat();
+  {
+    std::lock_guard l(heartbeat_lock);
+    heartbeat();
+  }
 
   // if our map within recent history, try to add ourselves to the osdmap.
   if (osdmap->get_epoch() == 0) {

ideepika/ceph-ci

(cherry picked from commit 7863d7c6c49347db97a6bca36f63fc35741ac0e5)
EC: 614007641415【IT商用版本测试】节点网络亚健康被隔离，被设置上noout nodown noup noin标签，手动取消这些标签后，有两个osd不能自动恢复
Change-Id: I44a012365f2d62e83c9e338848e99065bb58bfd3
Signed-off-by: 严军10111145 <yan.jun8@zte.com.cn>

src/osd/OSD.cc
@@ -5556,6 +5556,7 @@ void OSD::heartbeat_check()
 
 void OSD::heartbeat()
 {
+  ceph_assert(heartbeat_lock.is_locked_by_me());
   dout(30) << "heartbeat" << dendl;
 
   // get CPU load avg
@@ -6378,7 +6379,10 @@ void OSD::_preboot(epoch_t oldest, epoch_t newest)
 	   << oldest << ".." << newest << dendl;
 
   // ensure our local fullness awareness is accurate
-  heartbeat();
+  {
+    Mutex::Locker l(heartbeat_lock);
+    heartbeat();
+  }
 
   // if our map within recent history, try to add ourselves to the osdmap.
   if (osdmap->get_epoch() == 0) {